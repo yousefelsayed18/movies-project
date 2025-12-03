7c97a54ebeb9b5861061e1b02a8b7a3827c8b9ff
yousefelsayed18 <yousefabdelmonem18@gmail.com>
2025-12-03 22:33:59 +0200

    Fix navbar mobile toggle issue

--- src/Components/Nvbar/Navbar.jsx
+++ src/Components/Nvbar/Navbar.jsx
@@ -1,18 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Moviescontext } from "../../Contexts/Moviescontext";
 
 export default function Navbar() {
   let { searchByName } = useContext(Moviescontext);
+  const [open, setOpen] = useState(false);
 
   return (
     <nav className="bg-neutral-primary border-b border-default w-full z-20">
       <div className="max-w-7xl mx-auto flex items-center justify-between p-4">
         <button
-          data-collapse-toggle="navbar-search"
-          type="button"
+          onClick={() => setOpen(!open)}
           className="md:hidden inline-flex items-center p-2 text-sm rounded-lg hover:bg-neutral-secondary-soft"
-          aria-controls="navbar-search"
-          aria-expanded="false"
         >
           <svg
             className="w-6 h-6"
@@ -21,13 +19,16 @@ export default function Navbar() {
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
-            <path strokeLinecap="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+            <path
+              strokeLinecap="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
           </svg>
         </button>
+
         <div className="hidden md:block w-full">
           <div className="relative">
-
-
             <input
               onInput={(e) => searchByName(e.target.value)}
               type="text"
@@ -37,7 +38,8 @@ export default function Navbar() {
           </div>
         </div>
       </div>
-      <div id="navbar-search" className="hidden md:hidden px-4 pb-4">
+
+      <div className={`${open ? "block" : "hidden"} md:hidden px-4 pb-4`}>
         <input
           onInput={(e) => searchByName(e.target.value)}
           type="text"
